02364d96828be1d96c0ebfb87b65f4790a2ba475
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-09-04 15:12:05 +0300

    starting a project and using several tools to optimize the code

---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import heroes from "../reducers/heroes";
+import filters from "../reducers/filters";
+
+const stringMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store;
+}
+
+const store = createStore(combineReducers({heroes, filters}),
+    compose(applyMiddleware(stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
+
+export default store;
